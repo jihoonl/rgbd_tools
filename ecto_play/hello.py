626adae852641be3f4e6c035ba462e7e6e64f1d7
Jihoon Lee <jihoonlee.in@gmail.com>
2016-08-15 19:34:27 +0200

    ecto with catkin. and rosbag playter ready

---
+++ ecto_play/hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import ecto_tutorial.tutorial as tutorial
+
+# we get the cell that we named Hello in that module
+printer = tutorial.Hello()
+# execute the cell itself
+printer.process()
+
